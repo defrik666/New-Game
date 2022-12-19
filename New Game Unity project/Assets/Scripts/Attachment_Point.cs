58b86e934751612b2b2b93e41d1d893f12832ea2
defrik666 <defrik11@gmail.com>
2022-12-19 16:31:59 +0300

    Started working on gun system.

--- New Game Unity project/Assets/Scripts/Attachment_Point.cs
+++ New Game Unity project/Assets/Scripts/Attachment_Point.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Attachment_Point : MonoBehaviour
 {
-    public GameObject workArea;
+    public Transform workArea;
     private Camera_Interaction camInt;
 
     private void Start() {
         camInt = Camera.main.GetComponent<Camera_Interaction>();
+        //workArea = gameObject.transform.parent.parent;
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -16,22 +17,27 @@ public class Attachment_Point : MonoBehaviour
 
         if(attachment.CompareTag(gameObject.tag)){
             attachment.GetComponent<Attachment_Move>().pickedUp = false;
+            attachment.GetComponent<Attachment_Move>().target_pos = null;
             camInt.pickedUp = false;
 
             attachment.GetComponent<Rigidbody>().useGravity = false;
             attachment.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            attachment.GetComponent<Rigidbody>().isKinematic = true;
+            
 
+            attachment.transform.SetParent(gameObject.transform);
             attachment.transform.position = gameObject.transform.position;
-            attachment.transform.SetParent(gameObject.transform.parent);
+
 
             attachment.layer = LayerMask.NameToLayer("GunParts");
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        other.transform.SetParent(workArea.transform);
+        GameObject attachment = other.gameObject;
+        if(attachment.CompareTag(gameObject.tag)){
+            other.transform.SetParent(workArea);
+            attachment.GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
-
-
-
 }
